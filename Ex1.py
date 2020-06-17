0b5f29d588f54fe25d0657b4cfa22f704f3f5496
amnd1188 <36620205+amnd1188@users.noreply.github.com>
2020-06-17 17:13:38 +0530

    string

--- Ex1.py
+++ Ex1.py
@@ -26,3 +26,58 @@ while index < len(fruit):
     letter = fruit[index]
     print(index, letter)
     index = index + 1
+
+#looping and counting
+word = "coconut"
+count = 0
+for letter in word:
+    if letter == 'o':
+        count = count + 1
+print(count)
+
+#slicing string
+s = "useful python"
+print(s[0:3])
+
+print(s[4:5])
+
+print(s[:5])
+
+print(s[7:])
+
+print(s[:])
+
+#string concatenation
+x = "hello"
+y = "python"
+print(x + ' ' + y)
+
+#logical operator
+fruit = "banana"
+'n' in fruit
+'h' in fruit
+'ban' in fruit
+
+"""if 'nan' in fruit:
+    print('found it!')"""
+
+#string library,lower()
+greeting = "Hello Bob"
+x = greeting.lower()
+print(x)
+print('Hi There'.lower())
+
+#searching string,find()
+fruit = "banana"
+position = fruit.find('na')
+print(position)
+
+c = fruit.find('z')
+print(c)
+
+#search and replace,replace()
+greeting = "Hello Bob"
+x = greeting.replace('Bob','Jane')
+print(x)
+nst = greeting.replace('o','y')
+print(nst)
